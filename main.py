1291841626536d0a1849f26b28af552b53c45734
mohalw44 <mohalw04@gmail.com>
2025-03-16 23:04:01 +0300

    create game loop

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def main():
     print("starting asteroids")
     print(f"Screen size: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    BLACK = (0, 0, 0)
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(BLACK)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
